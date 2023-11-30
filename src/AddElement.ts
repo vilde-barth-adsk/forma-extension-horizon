0cf7bb5546ac2d311fd2f06bcc05b72a67da680f
vilde-barth-adsk <vilde.barth@autodesk.com>
2023-11-30 18:18:43 +0100

    Horizon

---
+++ src/AddElement.ts
@@ -0,0 +1,26 @@
+import { Forma } from "forma-embedded-view-sdk/auto";
+
+export async function addElement(position: number[], name?: string) {
+    const { urn } = await Forma.integrateElements.createElementHierarchy({
+        authcontext: Forma.getProjectId(),
+        data: {
+            rootElement: "root",
+            elements: {
+                root: {
+                    id: "root",
+                    properties: {
+                        name,
+                        geometry: {
+                            type: "Inline",
+                            format: "Mesh",
+                            verts: position,
+                            faces: Array(position.length / 3).fill(1).map((_, i) => i),
+                            doubleSided: true,
+                        },
+                    },
+                },
+            },
+        },
+    });
+    await Forma.proposal.addElement({ urn });
+}
--- src/app.tsx
+++ src/app.tsx
@@ -1,56 +1,12 @@
-import { useState } from "preact/hooks";
-import DateSelector from "./components/DateSelector";
-import ExportButton from "./components/ExportButton";
-import IntervalSelector from "./components/IntervalSelector";
-import ResolutionSelector from "./components/ResolutionSelector";
-import TimeSelector from "./components/TimeSelector";
-import PreviewButton from "./components/PreviewButton";
+import {Horizon} from "./components/Horizon.tsx";
 
 export default function App() {
-  const [month, setMonth] = useState(6);
-  const [day, setDay] = useState(15);
-  const [interval, setInterval] = useState(60);
-  const [startHour, setStartHour] = useState(8);
-  const [startMinute, setStartMinute] = useState(0);
-  const [endHour, setEndHour] = useState(20);
-  const [endMinute, setEndMinute] = useState(0);
-  const [resolution, setResolution] = useState("2048x1536");
 
   return (
     <>
-      <h1>Shadow study</h1>
-      <DateSelector month={month} setMonth={setMonth} day={day} setDay={setDay} />
-      <TimeSelector
-        startHour={startHour}
-        setStartHour={setStartHour}
-        startMinute={startMinute}
-        setStartMinute={setStartMinute}
-        endHour={endHour}
-        setEndHour={setEndHour}
-        endMinute={endMinute}
-        setEndMinute={setEndMinute}
-      />
-      <IntervalSelector interval={interval} setInterval={setInterval} />
-      <ResolutionSelector resolution={resolution} setResolution={setResolution} />
-      <PreviewButton
-        month={month}
-        day={day}
-        startHour={startHour}
-        startMinute={startMinute}
-        endHour={endHour}
-        endMinute={endMinute}
-        interval={interval}
-      />
-      <ExportButton
-        month={month}
-        day={day}
-        startHour={startHour}
-        startMinute={startMinute}
-        endHour={endHour}
-        endMinute={endMinute}
-        resolution={resolution}
-        interval={interval}
-      />
+      <h1>Horizon</h1>
+        <Horizon />
+
     </>
   );
 }
